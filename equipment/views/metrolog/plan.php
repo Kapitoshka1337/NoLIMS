168cfbdb03ca7e97aea35cbec911e7ea6247fdd3
Vladislav <djedai858@gmial.com>
2020-06-16 21:35:55 +0300

    Update plan
    Добавлено
     - Начало отображения для планов графиков

--- equipment/views/metrolog/plan.php
+++ equipment/views/metrolog/plan.php
@@ -3,57 +3,35 @@
     $this->registerJsFile('@web/assets/vendor/vue/axios.min.js');
     $this->registerJsFile('@web/assets/js/equipment/plan.js');
 ?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.1/moment.min.js"></script>
-<div class="column">
-  <div class="calendar">
-    <div class="title">
-      <div class="month">
-        {{monthName}}
-      </div>
-      <div class="year">
-        {{year}}
-      </div>
-    </div>
-    <ol class="days">
-      <li class="day" v-for="day in days | orderBy 'unix' 1" v-bind:class="{ 'outside': day.outsideOfCurrentMonth, 'empty': day.events.length === 0 }">
-        <div class="date">
-          <span class="weekday">{{day.weekday}}</span>
-          <span class="month">{{day.month}}</span>
-          <span class="day">{{day.number}}</span>
-          <span class="year">{{day.year}}</span>
-        </div>
-        <div class="events">
-          <div v-for="event in day.events" class="event" v-bind:class="{'first': event.firstInRange, 'last': event.lastInRange }">{{event.title}} </div>
-        </div>
-      </li>
-    </ol>
-  </div>
+<div class="row">
+	<div class="column">
+		<div class="ui fluid card">
+			<div class="content">
+				<button class="ui left floated orange button"><</button>
+				<button class="ui right floated orange button">></button>
+				<div class="header center aligned">Месяц</div>
+				<div class="meta center aligned">Год</div>
+			</div>
+			<div class="content">
+				<table class="ui compact table">
+					<thead>
+						<tr>
+							<th>Номер</th>
+							<th>Оборудование</th>
+							<th>Дата проверки</th>
+							<th>Вид проверки</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Номер</td>
+							<td>Оборудование</td>
+							<td>Дата проверки</td>
+							<td>Вид проверки</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </div>
-<div class="column" id="app">
-  <calendar></calendar>
-</div>
-<template id="calendar-template">
-  <div class="calendar">
-    <div class="title">
-      <div class="month">
-        {{monthName}}
-      </div>
-      <div class="year">
-        {{year}}
-      </div>
-    </div>
-    <ol class="days">
-      <li class="day" v-for="day in days | orderBy 'unix' 1" v-bind:class="{ 'outside': day.outsideOfCurrentMonth, 'empty': day.events.length === 0 }">
-        <div class="date">
-          <span class="weekday">{{day.weekday}}</span>
-          <span class="month">{{day.month}}</span>
-          <span class="day">{{day.number}}</span>
-          <span class="year">{{day.year}}</span>
-        </div>
-        <div class="events">
-          <div v-for="event in day.events" class="event" v-bind:class="{'first': event.firstInRange, 'last': event.lastInRange }">{{event.title}} </div>
-        </div>
-      </li>
-    </ol>
-  </div>
-</template>
