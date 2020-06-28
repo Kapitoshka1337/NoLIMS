b97a23a21fa9c182bf4a0b288fdb56122f78a3c1
Vladislav <djedai858@gmial.com>
2020-06-28 20:58:07 +0300

    Update metrolog index

--- equipment/views/metrolog/index.php
+++ equipment/views/metrolog/index.php
@@ -5,19 +5,70 @@
 ?>
 <div class="row" id="index">
 	<div class="sixteen column">
-		<equipment-dashboard :rows="cards"></equipment-dashboard>
+	<div class="ui two column grid">
+		<!-- <div class="column" v-for="row in cards"> -->
+			<equipment-dashboard :rows="helpEq" :count-post="countPost"></equipment-dashboard>
+			<equipment-dashboard :rows="testEq" :count-post="countPost"></equipment-dashboard>
+			<equipment-dashboard :rows="measuringEq" :count-post="countPost"></equipment-dashboard>
+			<equipment-dashboard :rows="maintenanceEq" :count-post="countPost"></equipment-dashboard>
+		<!-- </div> -->
+	</div>
 	</div>
 </div>
 <template id="equipment-dashboard">
-	<div class="ui two column grid">
-		<div class="column" v-for="row in rows">
-			<table class="ui compact table">
+<div class="column">
+	<table class="ui compact selectable table">
+		<thead>
+			<tr>
+				<th v-bind:colspan="rows.gridColumns.tableColumn.length + 1">
+					{{ rows.title }}
+					<a class="ui blue right floated mini icon button" v-on:click="printTable(rows.content[0].id_type)"><i class="icon print"></i></a>
+					<a class="ui red right floated mini icon button">{{rows.content.length}}</a>
+					<a class="ui orange right floated mini icon button">Всего</a>
+				</th>
+			</tr>
+			<tr>
+				<th v-for="key in rows.gridColumns.tableColumn">{{ Object.values(key)[0] }}</th>
+			</tr>
+		</thead>
+	<tbody>
+		<tr v-for="eq in filteredRows">
+			<td class="collapsing">{{ eq.card_number }}</td>
+			<td>{{ eq.equipment }}</td>
+			<td v-bind:class="{
+				success: colorShelfLife(eq.date_next_check) > 3,
+				caution: colorShelfLife(eq.date_next_check) <= 10 && colorShelfLife(eq.date_next_check) >= 16,
+				danger: colorShelfLife(eq.date_next_check) <= 6
+			}">{{ today(eq.date_next_check) }}</td>
+			<td class="right aligned">{{colorShelfLife(eq.date_next_check)}}</td>
+		</tr>
+	</tbody>
+		<tfoot>
+			<tr>
+				<th v-bind:colspan="rows.gridColumns.tableColumn.length + 1">
+					<div class="ui left floated label">Страница {{ currentPage }} из {{ listPages.length }}</div>
+					<div class="ui icon basic right floated small buttons">
+						<button class="ui button" v-on:click="currentPage = listPages[0]"><i class="icon angle double left"></i></button>
+						<button class="ui button" v-on:click="currentPage--" v-if="currentPage != 1"><i class="icon angle left"></i></button>
+						<div class="ui form">
+							<div class="field">
+							    <input type="text" v-model="currentPage" v-bind:value="currentPage">
+							</div>
+						</div>
+						<button class="ui button" v-on:click="currentPage++" v-if="currentPage < listPages.length"><i class="icon angle right"></i></button>
+						<button class="ui button" v-on:click="currentPage = listPages.length"><i class="icon angle double right"></i></button>
+					</div>
+				</th>
+			</tr>
+		</tfoot>
+	</table>
+</div>
+<!-- 			<table class="ui compact table">
 				<thead>
 					<tr>
 						<th colspan="4">
-							{{ row.title }}
-							<a class="ui blue right floated mini icon button" v-on:click="printTable(row.content[0].id_type)"><i class="icon print"></i></a>
-							<a class="ui red right floated mini icon button">{{row.content.length}}</a>
+							<a class="ui blue right floated mini icon button" v-on:click="printTable(rows[0].id_type)"><i class="icon print"></i></a>
+							<a class="ui red right floated mini icon button">{{rows.length}}</a>
 							<a class="ui orange right floated mini icon button">Всего</a>
 						</th>
 					</tr>
@@ -29,7 +80,7 @@
 					</tr>
 				</thead>
 				<tbody>
-					<tr v-for="eq in row.content">
+					<tr v-for="eq in rows">
 						<td class="collapsing">{{ eq.card_number }}</td>
 						<td>{{ eq.equipment }}</td>
 						<td v-bind:class="{
@@ -40,7 +91,6 @@
 						<td class="right aligned">{{colorShelfLife(eq.date_next_check)}}</td>
 					</tr>
 				</tbody>
-			</table>
-		</div>
-	</div>
+			</table> -->
+
 </template>
--- js/equipment/metrolog_index.js
+++ js/equipment/metrolog_index.js
@@ -2,17 +2,35 @@ Vue.config.devtools = true;
 Vue.component('equipment-dashboard', {
 	template: '#equipment-dashboard',
 	props: {
-		rows: Array
+		rows: Object,
+		// columns: Array,
+		countPost: Number
 	},
-	data: function () {},
+	data: function () {
+		return {
+			currentPage: 1,
+			listPages: []
+		}
+	},
 	methods: {
+		setPages() {
+			let numOfPage = Math.ceil(this.rows.content.length / this.countPost);
+			for (let i = 1; i <= numOfPage; i++)
+				this.listPages.push(i);
+		},
+		paginate(rows) {
+			let page = this.currentPage;
+			let curPost = this.countPost;
+			let from = (page * curPost) - curPost;
+			let to = ((page * curPost));
+			return rows.slice(from, to);
+		},
 		today(date){
 			if(date === null) return;
 			let today = new Date(date);
 			return today.toLocaleString().split(',')[0];
 		},
 		printTable(id_type){
-			alert(id_type);
 			let date = new Date();
 			let objs = {start: new Date(date.getFullYear(), date.getMonth(), 2).toISOString().split('T')[0], end: new Date(date.getFullYear(), date.getMonth() + 1, 1).toISOString().split('T')[0], type: id_type};
 			axios.post("/equipment/print-table?today", JSON.stringify(objs), {headers: {'Content-Type': 'application/json'}}).then( response => (window.open('/assets/template/plan.pdf')));
@@ -23,22 +41,86 @@ Vue.component('equipment-dashboard', {
 			return Math.ceil((date1.getTime() - today.getTime()) / (1000 * 3600 * 24));
 		},
 	},
+	watch: {
+		filteredRows() {
+			this.listPages = [];
+			this.setPages();
+		}
+	},
+	computed: {
+		filteredRows(){
+			let rows = this.rows.content;
+			return this.paginate(rows);
+		}
+	}
 })
 
 let index = new Vue({
 	el: "#index",
 	data: {
-		cards: [
-			{title: 'Вспомогательное оборудование', content: []},
-			{title: 'Испытательное оборудование', content: []},
-			{title: 'Средства измерения', content: []},
-			{title: 'Техническое обслуживание', content: []}
-		],
-		gridData: []
+		helpEq:{
+				title: 'Вспомогательное оборудование',
+				gridColumns: {
+					tableColumn: [
+						{'card_number':'Номер'},
+						{'equipment':'Оборудование'},
+						{'date_next_check':'Проверка'},
+						{'action': ''}
+					]
+				},
+				content: []
+			},
+		testEq:{
+				title: 'Испытательное оборудование',
+				gridColumns: {
+					tableColumn: [
+						{'card_number':'Номер'},
+						{'equipment':'Оборудование'},
+						{'date_next_check':'Проверка'},
+						{'action': ''}
+					]
+				},
+				content: []
+			},
+		measuringEq:{
+				title: 'Средство измерений',
+				gridColumns: {
+					tableColumn: [
+						{'card_number':'Номер'},
+						{'equipment':'Оборудование'},
+						{'date_next_check':'Проверка'},
+						{'action': ''}
+					]
+				},
+				content: []
+			},
+		maintenanceEq:{
+				title: 'Техническое обслуживание',
+				gridColumns: {
+					tableColumn: [
+						{'card_number':'Номер'},
+						{'equipment':'Оборудование'},
+						{'date_next_check':'Проверка'},
+						{'action': ''}
+					]
+				},
+				content: []
+			},
+		gridData: [],
+		countPost: 10
 	},
 	watch: {
 		gridData(){
-			this.gridData.filter(f => {this.cards[f.id_type - 1].content.push(f);})
+			this.gridData.filter(f => {
+				if(f.id_type === 1)
+					this.helpEq.content.push(f);
+				if(f.id_type === 2)
+					this.testEq.content.push(f);
+				if(f.id_type === 3)
+					this.measuringEq.content.push(f);
+				if(f.id_type === 4)
+					this.maintenanceEq.content.push(f);
+			})
 		}
 	},
 	methods:{
