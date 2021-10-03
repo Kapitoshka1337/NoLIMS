e8d73264d12ca71e163a480ecd2819e52bbcb4dd
starkov_vv <djedai858@gmail.com>
2021-10-03 08:56:49 +0400

    Изменена таблица Местоположение

--- Application/DTOs/Base/Location.cs
+++ Application/DTOs/Base/Location.cs
@@ -2,10 +2,7 @@
 {
     public class Location : BaseDto
     {
-        public string Name { get; set; }
         public string NumberRoom { get; set; }
-        public string Notation { get; set; }
-        public string Storage { get; set; }
         public int DepartmentId { get; set; }
     }
 }
