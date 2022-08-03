18789b670f47d8563de7337e907ecdda7f8ec063
Starkov_VV <djefai858@gmail.com>
2022-08-03 13:11:33 +0400

    Оборудование объеденено в одно

---
+++ Infrastructure.Persistence/Migrations/20220803082823_JoinEquipment.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class JoinEquipment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Characteristics",
+                table: "Equipment");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Equipment");
+
+            migrationBuilder.DropColumn(
+                name: "EquipmentIO_Accuracy",
+                table: "Equipment");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Characteristics",
+                table: "Equipment",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Equipment",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EquipmentIO_Accuracy",
+                table: "Equipment",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
---
+++ Infrastructure.Persistence/Migrations/20220803083612_JoinEquipmentV2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class JoinEquipmentV2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Characteristics",
+                table: "Equipment",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Characteristics",
+                table: "Equipment");
+        }
+    }
+}
