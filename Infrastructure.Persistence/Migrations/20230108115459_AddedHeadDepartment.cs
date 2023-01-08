be6a78d73c496a10d8b6a8588472630d6f6a8823
StarkovVV18 <v.starckov2012@yandex.ru>
2023-01-08 16:28:33 +0400

    Миграция головного подразделения

---
+++ Infrastructure.Persistence/Migrations/20230108115459_AddedHeadDepartment.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class AddedHeadDepartment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HeadDepartmentId",
+                table: "Departments",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_HeadDepartmentId",
+                table: "Departments",
+                column: "HeadDepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Departments_HeadDepartmentId",
+                table: "Departments",
+                column: "HeadDepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Departments_HeadDepartmentId",
+                table: "Departments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Departments_HeadDepartmentId",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "HeadDepartmentId",
+                table: "Departments");
+        }
+    }
+}
