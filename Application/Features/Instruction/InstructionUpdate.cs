aebe8d8aba453a885d6ce7859dfbb5bb7b264551
StarkovVV18 <v.starckov2012@yandex.ru>
2023-01-09 14:23:45 +0400

    Отправка обновленной инструкции вместо ИД

--- Application/Features/Instruction/InstructionUpdate.cs
+++ Application/Features/Instruction/InstructionUpdate.cs
@@ -4,10 +4,11 @@ using AutoMapper;
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Features.Instruction.GetAll;
 
 namespace Application.Features.Instruction
 {
-    public class InstructionUpdate : IRequest<Response<int>>
+    public class InstructionUpdate : IRequest<Response<ViewModel>>
     {
         public string Name { get; set; }
         public int Id { get; set; }
@@ -15,7 +16,7 @@ namespace Application.Features.Instruction
         public int FileId { get; set; }
     }
 
-    public class InstuctionUpdateHandler : IRequestHandler<InstructionUpdate, Response<int>>
+    public class InstuctionUpdateHandler : IRequestHandler<InstructionUpdate, Response<ViewModel>>
     {
         private readonly IInstructionRepository _repository;
         private readonly IMapper _mapper;
@@ -25,13 +26,15 @@ namespace Application.Features.Instruction
             _mapper = mapper;
         }
 
-        public async Task<Response<int>> Handle(InstructionUpdate request, CancellationToken cancellationToken)
+        public async Task<Response<ViewModel>> Handle(InstructionUpdate request, CancellationToken cancellationToken)
         {
             var equipmentDto = _mapper.Map<DTOs.Equipment.Instruction>(request);
             var equipmentBase = _mapper.Map<Domain.Entities.Equipment.Instruction>(equipmentDto);
             await _repository.UpdateAsync(equipmentBase);
+
+            var equipmentViewModel = _mapper.Map<ViewModel>(equipmentBase);
             
-            return new Response<int>(equipmentBase.Id);
+            return new Response<ViewModel>(equipmentViewModel);
         }
     }
 }
